e971724e5f99a1a1d8831eb0e90b00ab209e52ac
Az1zbekx <kalilinux42331@gmail.com>
2025-04-09 12:17:44 +0500

    Game

---
+++ game.py
@@ -0,0 +1,20 @@
+import random
+
+print("1 dan 1000 oraliqdagi istalgan son kiriting")
+
+x = random.randint(1, 1000)
+
+while True:
+
+    y = int(input())
+
+    if y > x :
+        print("Siz katta son kiritdingiz")
+    elif y < x :
+        print("Siz kichik son kiritdingiz")
+    else:
+        print("Siz sonni topdingiz")
+        break
+
+
+
